78e1129be0ee9f3bad8f89fbfec23e8c4364043c
shinas07 <shinasamanpk@gmail.com>
2025-03-07 16:54:28 +0530

    Refactor: Wrapped App with AuthProvider for authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import router from './routes/router'
 import { Toaster } from "sonner";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div>
-      <Toaster position="top-right"/>
+    <div className="min-h-screen">
+      <AuthProvider>
+      <Toaster position="top-right" expand={false} richColors />
       <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   );
 }
